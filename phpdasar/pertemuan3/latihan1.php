5b5912b8415b52da46364a53a6c19478468dd7a9
hermawanyuniarto <hermawanyuniarto@gmail.com>
2021-09-17 13:37:54 +0700

    5. STRUKTUR KENDALI
    pertemuan 3
    berhenti di video di time 14:50

---
+++ phpdasar/pertemuan3/latihan1.php
@@ -0,0 +1,29 @@
+<?php
+// Pengulangan
+// for
+// while
+// do.. while
+// foreach : Pengulangan khusus array
+
+// for ($i=0; $i < 5; $i++) { 
+//     echo "Hello World! <br>";
+// }
+
+// $i = 0;
+// while ($i < 5) {
+//     echo "Hello World! <br>";
+//     $i++;
+// }
+
+// $i = 0;
+// do {
+//     echo "Hello World! <br>";
+//     $i++;
+// } while ($i < 5);
+
+// $i = 10;
+// do {
+//     echo "Hello World! <br>";
+//     $i++;
+// } while ($i < 5);
+?>
