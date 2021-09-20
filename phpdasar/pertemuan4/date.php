bb0476440cf9a864ae71acf48256055e5f2b5695
hermawanyuniarto <hermawanyuniarto@gmail.com>
2021-09-20 18:10:33 +0700

    menghitung 100 hari kebelakang dari sekarang

--- phpdasar/pertemuan4/date.php
+++ phpdasar/pertemuan4/date.php
@@ -7,5 +7,6 @@
     // UNIX Timestamp / EPOCH time
     // detik yang sudah berlalu sejak 1 Januari 1970
     // echo time();
-    echo date("l", time()+60*60*24*100);
+    // echo date("l", time()+60*60*24*100);
+    echo date("d M Y", time()-60*60*24*100);
 ?>
