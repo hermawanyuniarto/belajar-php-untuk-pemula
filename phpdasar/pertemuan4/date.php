340169dcc5fb8c3f6c7f1e1927f4c3ec99314bfe
hermawanyuniarto <hermawanyuniarto@gmail.com>
2021-09-20 18:37:38 +0700

    membuat function mktime, mencari hari lahir

--- phpdasar/pertemuan4/date.php
+++ phpdasar/pertemuan4/date.php
@@ -8,5 +8,11 @@
     // detik yang sudah berlalu sejak 1 Januari 1970
     // echo time();
     // echo date("l", time()+60*60*24*100);
-    echo date("d M Y", time()-60*60*24*100);
+    // echo date("d M Y", time()-60*60*24*100);
+
+    // mktime
+    // membuat sendiri detik
+    // mktime (0,0,0,0,0,0)
+    // jam, menit, detik, bulan, tanggal, tahun
+    echo date("l", mktime(0,0,0,6,24,1982));
 ?>
