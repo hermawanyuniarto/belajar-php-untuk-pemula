bff33edd6a836fc610886ea63d2e16ddad05cfa6
hermawanyuniarto <hermawanyuniarto@gmail.com>
2021-09-20 18:06:53 +0700

    menampilkan 100 hari dari sekarang

--- phpdasar/pertemuan4/date.php
+++ phpdasar/pertemuan4/date.php
@@ -1,5 +1,11 @@
 <?php
     // date
     // untuk menampilkan tanggal dengan format tertentu
-    echo date("l, d-M-Y");
+    // echo date("l, d-M-Y");
+
+    // time
+    // UNIX Timestamp / EPOCH time
+    // detik yang sudah berlalu sejak 1 Januari 1970
+    // echo time();
+    echo date("l", time()+60*60*24*100);
 ?>
