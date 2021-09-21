954136cc113f22b8fd632c7abb3c2282cab33f9a
hermawanyuniarto <hermawanyuniarto@gmail.com>
2021-09-22 05:05:07 +0700

    menambahkan pertemuan 5 latihan 1 - menampilkan dgn menggunakan var_dump

---
+++ phpdasar/pertemuan5/latihan1.php
@@ -0,0 +1,19 @@
+<?php
+    // array
+    // variabel yang dapat memiliki banyak nilai
+    // elemen pada array boleh memiliki tipe data yang berbeda
+    // pasangan antara key dan value
+    // key-nya adalah index, yang selalu dimulai dari 0
+
+    // membuat array
+    // cara lama
+    $hari = array("Senin", "Selasa", "rabu");
+    // cara baru
+    $bulan = ["Januari", "Februari", "Maret"];
+    $arr1 = [123, "tulisan", false];
+
+    // menampilkan array
+    // var_dump() / print_r()
+    var_dump($hari);
+
+?>
