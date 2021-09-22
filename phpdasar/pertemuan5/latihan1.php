35f07cdb7b1baa0e7f5aa8add79bf39e0e3df81e
hermawanyuniarto <hermawanyuniarto@gmail.com>
2021-09-22 14:31:50 +0700

    menampilkan elemen baru pada array

--- phpdasar/pertemuan5/latihan1.php
+++ phpdasar/pertemuan5/latihan1.php
@@ -14,15 +14,21 @@
 
     // menampilkan array
     // var_dump() / print_r()
-    var_dump($hari);
-    echo "<br>";
+    // var_dump($hari);
+    // echo "<br>";
     
-    print_r($bulan);
-    echo "<br>";
+    // print_r($bulan);
+    // echo "<br>";
 
     // menampilkan 1 elemen pada array
-    echo $arr1 [0];
-    echo "<br>";
-    echo $bulan [1];
+    // echo $arr1 [0];
+    // echo "<br>";
+    // echo $bulan [1];
 
+    // menampilkan elemen baru pada array
+    var_dump($hari);
+    $hari [] = "Kamis";
+    $hari [] = "Jum'at";
+    echo "<br>";
+    var_dump($hari);
 ?>
