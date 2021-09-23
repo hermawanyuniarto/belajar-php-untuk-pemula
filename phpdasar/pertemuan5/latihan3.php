644a9a48d2eb26da03ce968753f04238b7c28b8b
hermawanyuniarto <hermawanyuniarto@gmail.com>
2021-09-23 13:23:28 +0700

    membuat data mahasiswa dgn - metode foreach

---
+++ phpdasar/pertemuan5/latihan3.php
@@ -0,0 +1,26 @@
+<?php
+$mahasiswa = ["Hermawan Yuniarto", "04519820624", "Sistem Informasi", "hermawanyuniarto@gmail.com"];
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daftar Mahasiswa</title>
+</head>
+
+<body>
+
+    <h1>Daftar Mahasiswa</h1>
+
+    <ul>
+        <?php foreach ($mahasiswa as $m) : ?>
+            <li><?= $m; ?></li>
+        <?php endforeach; ?>
+    </ul>
+
+</body>
+
+</html>
