8149ed342c90ff8ba5f1296192f087ac98571857
hermawanyuniarto <hermawanyuniarto@gmail.com>
2021-09-23 15:14:14 +0700

    menambahkan - video | 8. ASSOCIATIVE ARRAY menit 10:19

---
+++ phpdasar/pertemuan6/latihan1.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan Array</title>
+    <style>
+        .kotak {
+            width: 30px;
+            height: 30px;
+            background-color: greenyellow;
+            text-align: center;
+            line-height: 30px;
+            margin: 3px;
+            float: left;
+            transition: 1s;
+        }
+
+        .kotak:hover {
+            transform: rotate(360deg);
+            border-radius: 50%;
+        }
+    </style>
+</head>
+
+<body>
+
+    <div class="kotak">5</div>
+
+</body>
+
+</html>
