155b76a7861e28b98913006378463135f23f8707
hermawanyuniarto <hermawanyuniarto@gmail.com>
2021-09-26 16:25:16 +0700

    menggunakan foreach

--- phpdasar/pertemuan6/latihan1.php
+++ phpdasar/pertemuan6/latihan1.php
@@ -27,7 +27,13 @@
 
 <body>
 
-    <div class="kotak">5</div>
+<?php 
+$angka = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+?>
+
+<?php foreach( $angka as $a ) : ?>
+    <div class="kotak"><?= $a; ?></div>
+<?php endforeach; ?>
 
 </body>
 
