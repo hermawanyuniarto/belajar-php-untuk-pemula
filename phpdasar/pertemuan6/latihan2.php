2db598dd1e4c446068c8fab0ff1e55907bcd700d
hermawanyuniarto <hermawanyuniarto@gmail.com>
2021-09-28 13:14:46 +0700

    pertemun 6 latihan2.php - mengambil data mahasiswa dari array

---
+++ phpdasar/pertemuan6/latihan2.php
@@ -0,0 +1,26 @@
+<?php
+$mahasiswa = ["Hermawan Yuniarto", "2002240682", "hermawanyuniarto@gmail.com", "Sistem Informatika"]
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daftar Mahasiswa</title>
+</head>
+
+<body>
+    <h1>Daftar Mahasiswa</h1>
+
+    <ul>
+        <li><?= $mahasiswa[0]; ?></li>
+        <li><?= $mahasiswa[1]; ?></li>
+        <li><?= $mahasiswa[2]; ?></li>
+        <li><?= $mahasiswa[3]; ?></li>
+    </ul>
+</body>
+
+</html>
